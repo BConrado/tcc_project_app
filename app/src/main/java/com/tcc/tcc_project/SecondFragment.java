357b0ff107940d09fde53bb324f15673d8262e09
Felipe3Ferro <felipe.nascimento00@edu.pucrs.br>
2022-11-15 03:57:23 -0300

    integrando 1

--- app/src/main/java/com/tcc/tcc_project/SecondFragment.java
+++ app/src/main/java/com/tcc/tcc_project/SecondFragment.java
@@ -62,7 +62,7 @@ public class SecondFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
-
+                ((MainActivity3)getActivity()).stopTimer();
             }
         });
         chart = getView().findViewById(R.id.InfoChat1);
